9070813c15e0534c44a894ed3e17964841d2da08
Katarzyna Barmuta <katarzyna.barmuta94@gmail.com>
2023-04-26 15:58:34 +0200

    feat: add button to the keyboard

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const input = document.createElement("input");
-input.type = "text";
-input.className = "input-text";
+const textarea = document.createElement("textarea");
+textarea.type = "text";
+textarea.className = "textarea-text";
 
 const parent = document.createElement("div");
 parent.className = "container";
-parent.appendChild(input);
+parent.appendChild(textarea);
 document.body.appendChild(parent);
 
 const keyboardWrapper = document.createElement("div");
@@ -15,4 +15,20 @@ const keyboardContainer = document.createElement("div");
 keyboardContainer.className = "keyboard-container";
 keyboardWrapper.appendChild(keyboardContainer);
 
+const keys = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+    'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', ' \\ ', 'DEL',
+    'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter',
+    'Shift', ' \\ ', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '.', ',', '/', '▲', 'Shift',
+    'Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', '◄', '▼', '►'];
 
+function init() {
+    let output = '';
+    for (let i = 0; i < keys.length; i++) {
+        if (i === 14 || i === 29 || i === 42 || i === 56) {
+            output += `<div class="clearfix"></div>`;
+        }
+        output += `<button class="key-button" >${keys[i]}</button>`;
+    }
+    document.querySelector('.keyboard-container').innerHTML = output;
+}
+init();
